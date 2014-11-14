1ac7b94b2684e295ea40298e74f4a2e9deb0ce75
picsouman <aimeric.01@hotmail.fr>
2014-11-14 23:22:20 +0100

    Validation of Class Organisation

--- CatsOvercome/Assets/Scripts/Character.cs
+++ CatsOvercome/Assets/Scripts/Character.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 
+    #region "Attributes"
+
     string Username = "Meow Clawson";
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    #endregion
+
+    #region "Constructor/Init"
+
+    #endregion
+
+    #region "Events"
+
+    #endregion
+
+    #region "Methods"
+
+    #endregion
+
 }
